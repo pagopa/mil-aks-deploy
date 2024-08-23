3905a62bffa87188631596833665e6fca639f38c
Luca Consalvi <luca.consalvi@pagopa.it>
2024-08-23 09:34:15 +0200

    add mil-preset helm values

---
+++ helm/dev/mil-preset/deploy.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+../../../scripts/deploy.sh values.yaml mil cstar-d-weu-dev01-aks mil-preset
---
+++ helm/uat/mil-preset/deploy.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+../../../scripts/deploy.sh values.yaml mil cstar-u-weu-dev01-aks mil-preset
